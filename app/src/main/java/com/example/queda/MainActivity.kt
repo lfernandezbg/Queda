63938f81c7fe063ce6e458b7f9ce1f56b612223a
lfern <lfernandezbg@gmail.com>
2026-07-14 15:22:07 +0200

    .

--- app/src/main/java/com/example/queda/MainActivity.kt
+++ app/src/main/java/com/example/queda/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             QuedaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android_test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
