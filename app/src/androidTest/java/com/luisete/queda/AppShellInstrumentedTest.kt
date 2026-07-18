12104b651b8e97499ac3b90d4101210aed617989
lfern <lfernandezbg@gmail.com>
2026-07-18 23:57:17 +0200

    style(app): format navigation instrumented test

--- app/src/androidTest/java/com/luisete/queda/AppShellInstrumentedTest.kt
+++ app/src/androidTest/java/com/luisete/queda/AppShellInstrumentedTest.kt
@@ -2,12 +2,13 @@ package com.luisete.queda
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import com.luisete.queda.feature.inventory.InventoryTestTags
 import org.junit.Rule
 import org.junit.Test
-import androidx.compose.ui.test.onAllNodesWithTag
+
 class AppShellInstrumentedTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
@@ -18,7 +19,7 @@ class AppShellInstrumentedTest {
         composeTestRule.onNodeWithTag(InventoryTestTags.INVENTORY_SCREEN).assertIsDisplayed()
 
         // Tap Add
-        //composeTestRule.onNodeWithTag(InventoryTestTags.INVENTORY_ADD_BUTTON).performClick()
+        // composeTestRule.onNodeWithTag(InventoryTestTags.INVENTORY_ADD_BUTTON).performClick()
         // Wait until Inventory finishes loading and exposes the Add action.
         composeTestRule.waitUntil(timeoutMillis = 30_000) {
             composeTestRule
